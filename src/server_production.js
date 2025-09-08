1e93566ce0402e1646f004dfb9f921b51caa8272
Mehul Patel <swatmehul67@gmail.com>
2025-09-08 09:00:07 +0530

    Update server_production.js

--- src/server_production.js
+++ src/server_production.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -234,8 +237,4 @@ bot.on('callback_query', async cb => {
 });
 
 // ===== START SERVER =====
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
-                               // or ye rha mera server.js code const express = require('express');
-                                   
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
